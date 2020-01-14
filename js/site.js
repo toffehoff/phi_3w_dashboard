79d6d098c49ed209e7086fe16518c305fc6787b0
Henk Hoff <toffehoff@gmail.com>
2020-01-14 21:56:34 +0800

    width graph - map back to 6-6

--- js/site.js
+++ js/site.js
@@ -85,7 +85,7 @@ d3.dsv(';')("data/3W_Data.csv", function(csv_data) {
 				.xAxis().ticks(5) // added
 				;
 
- 	service_chart.width(320).height(300)
+ 	service_chart.width(500).height(300)
                 .dimension(xf.service)
                 .group(service)
                 .elasticX(true)
@@ -280,8 +280,8 @@ d3.dsv(';')("data/3W_Data.csv", function(csv_data) {
 	//Determine initial settings for the projection:
       var center = d3.geo.centroid(obj_provincesJSON)
 	  var scale  = 150;
-	  var width = 660;
-      var height  = 700;
+	  var width = 550;
+      var height  = 600;
       var offset = [width/2, height/2];
       var projection = d3.geo.mercator().scale(scale).center(center).translate(offset);
 
@@ -305,7 +305,7 @@ d3.dsv(';')("data/3W_Data.csv", function(csv_data) {
       path = path.projection(projection);
 
 	//Define the map settings for the provinces:
-	map_chart.width(660).height(700)
+	map_chart.width(550).height(600)
 	.dimension(xf.pcode)
 	.group(pcode)
 	.colors(d3.scale.quantile()
@@ -407,8 +407,8 @@ d3.dsv(';')("data/3W_Data.csv", function(csv_data) {
 	  //Ideally the dimensions of the provinces map are the target:
       var center = d3.geo.centroid(obj_provincesJSON)
       var scale  = 150;
-	  var width = 660;
-	  var height = 700;
+	  var width = 550;
+	  var height = 600;
       var offset = [width/2, height/2];
       var projection = d3.geo.mercator().scale(scale).center(center)
           .translate(offset);
@@ -432,7 +432,7 @@ d3.dsv(';')("data/3W_Data.csv", function(csv_data) {
 
 
 	//Define the map settings for the provinces:
-	map2_chart.width(660).height(700)
+	map2_chart.width(550).height(600)
     .dimension(xf.mcode)
     .group(mcode)
 	.colors(d3.scale.quantile()
